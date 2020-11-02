f27592477f2f0e271bb541813d7d5796b9390aa7
Nightara <webmin@nightara.de>
2020-11-02 15:23:21 +0100

    Added JavaDoc comments to all model classes.

--- src/main/java/de/dhbw/swe/betterjeweled/core/CrystalGrid.java
+++ src/main/java/de/dhbw/swe/betterjeweled/core/CrystalGrid.java
@@ -7,10 +7,39 @@ import java.util.List;
 import java.util.function.*;
 import java.util.stream.*;
 
+/**
+ * The CrystalGrid class represents a rectangular grid of variable size containing any number of crystals within its
+ * bounds.
+ * <br />
+ * The class offers basic functionality like group detection and general methods required for grid manipulation, like
+ * deletion of groups, switch of neighboring crystal, random refill of empty spaces, and gravitational pull. The grid
+ * does not contain any game logic or order of actions, it merely acts as a modifiable two-dimensional container for
+ * Crystal objects.
+ * <br />
+ * The number and list of valid Crystals for any grid as well as its size is set during creation and can not be modified
+ * afterwards.
+ * <br />
+ * Any CrystalGrid holds a private Random object that will be used for any required randomness. Creating two CrystalGrid
+ * objects with the same size, color list and seeds for their respective Random objects results in two similar grids
+ * that can be expected to behave equally if treated the same way. Currently the only method calling the Random object
+ * is CrystalGrid#fillGrid.
+ * <br />
+ * The grid coordinates are recommended to be interpreted like monitor coordinates, with x depicting the horizontal
+ * axis, y depicting the vertical axis, and [0, 0] representing the top left corner. The x coordinate can be inverted
+ * without causing any problems, but it is strongly recommended to not invert the y axis, because the default
+ * implementation of the CrystalGrid#shiftCrystals "gravitation" method shifts every Crystal towards positive y and all
+ * empty fields towards negative y.
+ */
 @Value
 public class CrystalGrid
 {
+  /**
+   * The constant TRIGGER_SIZE.
+   */
   public static final int TRIGGER_SIZE = 3;
+  /**
+   * The constant SCORE_BASE.
+   */
   public static final int SCORE_BASE = 100;
 
   int sizeX;
@@ -18,6 +47,13 @@ public class CrystalGrid
   Crystal[] colors;
   Crystal[][] field;
 
+  /**
+   * The default constructor.
+   *
+   * @param sizeX  The x (Horizontal) size of the grid
+   * @param sizeY  The y (Vertical) size of the grid
+   * @param colors The colors available for the grid
+   */
   public CrystalGrid(int sizeX, int sizeY, Crystal... colors)
   {
     this.sizeX = sizeX;
@@ -26,13 +62,22 @@ public class CrystalGrid
     this.field = new Crystal[sizeX][sizeY];
   }
 
+  /**
+   * Returns the total number of fields contained in the grid.
+   *
+   * @return The size of the grid
+   */
   public int getSize()
   {
     return getSizeX() * getSizeY();
   }
 
   /**
+   * Returns the Crystal stored at the supplied coordinates.
    *
+   * @param posX The x (Horizontal) coordinate of the crystal
+   * @param posY The y (Vertical) coordinate of the crystal
+   * @return The requested Crystal or Optional#empty if none is present
    * @throws IndexOutOfBoundsException if the supplied coordinates are outside of the grid.
    */
   public Optional<Crystal> getCrystal(int posX, int posY)
@@ -48,7 +93,12 @@ public class CrystalGrid
   }
 
   /**
+   * Sets a supplied Crystal at the supplied coordinates. This method does not check for duplication or validity of the
+   * supplied Crystal for this grid and should be used with caution.
    *
+   * @param posX    The x (Horizontal) coordinate of the crystal
+   * @param posY    The y (Vertical) coordinate of the crystal
+   * @param crystal The Crystal object to be placed
    * @throws IndexOutOfBoundsException if the supplied coordinates are outside of the grid.
    */
   public void setCrystal(int posX, int posY, Crystal crystal)
@@ -56,6 +106,13 @@ public class CrystalGrid
     getField()[posX][posY] = crystal;
   }
 
+  /**
+   * Fills all empty fields of this grid by randomly picking crystals from this grid's color list using the internal
+   * Random object. Like CrystalGrid#setCrystal, this method does not create copies of the reference crystals, but
+   * simply creates a new reference to those objects instead.
+   *
+   * @return The total number of modified fields
+   */
   public int fillGrid()
   {
     return fillGrid(new Random());
@@ -80,7 +137,16 @@ public class CrystalGrid
   }
 
   /**
+   * Switches two adjacent fields, returning true if the requested action resulted in a change of the grid, and false
+   * otherwise. Any switch request that is within bounds of the grid and targets two fields that are orthogonally
+   * adjacent to each other is considered to be a "change" of the grid, even if the two targeted fields contain the same
+   * type of crystal (Or null).
    *
+   * @param posXOne The x (Horizontal) coordinate of the first crystal
+   * @param posYOne The y (Vertical) coordinate of the first crystal
+   * @param posXTwo The x (Horizontal) coordinate of the first crystal
+   * @param posYTwo The y (Vertical) coordinate of the first crystal
+   * @return True if the request resulted in a change of the grid, false otherwise
    * @throws IndexOutOfBoundsException if the supplied coordinates are outside of the grid.
    */
   public boolean switchCrystals(int posXOne, int posYOne, int posXTwo, int posYTwo)
@@ -99,6 +165,11 @@ public class CrystalGrid
     return false;
   }
 
+  /**
+   * Detects all regions of at least TRIGGER_SIZE length sorted by crystal type and returns them as a map of lists.
+   *
+   * @return All regions in the current grid
+   */
   public Map<Crystal, List<CrystalRegion>> findRegions()
   {
     return Arrays.stream(getColors()).collect(Collectors.toMap(Function.identity(), color ->
@@ -151,6 +222,13 @@ public class CrystalGrid
     }));
   }
 
+  /**
+   * Detects all regions using CrystalGrid#findRegions and deleted all crystals contained within them, returning the
+   * total score for the creation of these regions according to CrystalGrid#scoreRegion. Crystals are deleted from left
+   * to right, then top to bottom.
+   *
+   * @return The total scores for the deleted regions
+   */
   public int triggerRegions()
   {
     return findRegions().values().stream()
@@ -171,6 +249,18 @@ public class CrystalGrid
         }).sum();
   }
 
+  /**
+   * Scores a supplied region. This method is expected to always fulfill the following requirements:<br />
+   * <ol>
+   *   <li>Regions shorter than TRIGGER_SIZE receive a score of zero.</li>
+   *   <li>Regions that can be converted into each other by movement, mirroring and / or rotation receive the same
+   *   score.</li>
+   *   <li>Regions of size x receive a higher score than two regions of size x - 1.</li>
+   * </ol>
+   *
+   * @param region The region to score
+   * @return A score for deleting the supplied region
+   */
   public static int scoreRegion(CrystalRegion region)
   {
     if(region.getSize() < TRIGGER_SIZE)
--- src/main/java/de/dhbw/swe/betterjeweled/core/CrystalRegion.java
+++ src/main/java/de/dhbw/swe/betterjeweled/core/CrystalRegion.java
@@ -2,6 +2,10 @@ package de.dhbw.swe.betterjeweled.core;
 
 import lombok.*;
 
+/**
+ * The CrystalRegion class represents a simple rectangular shape and offers utility functionality to check whether a
+ * field is contained within this region.
+ */
 @Value
 public class CrystalRegion
 {
@@ -10,11 +14,23 @@ public class CrystalRegion
   int endX;
   int endY;
 
+  /**
+   * Returns whether the field with the supplied coordinates are contained within this region.
+   *
+   * @param posX The x (Horizontal) coordinate
+   * @param posY The y (Vertical) coordinate
+   * @return True if the supplied position is contained within this region, false otherwise
+   */
   public boolean contains(int posX, int posY)
   {
     return posX >= getStartX() && posX < getEndX() && posY >= getStartY() && posY < getEndY();
   }
 
+  /**
+   * Returns the size of this region.
+   *
+   * @return The size of this region
+   */
   public int getSize()
   {
     return Math.abs(getStartX() - getEndX()) * Math.abs(getStartY() - getEndY());
