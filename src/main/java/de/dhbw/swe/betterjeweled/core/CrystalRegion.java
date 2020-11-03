830e85933d9e74dc0847ae0cce478fb93a539351
Nightara <webmin@nightara.de>
2020-11-03 14:04:03 +0100

    Added JavaDoc examples to CrystalRegion.

--- src/main/java/de/dhbw/swe/betterjeweled/core/CrystalRegion.java
+++ src/main/java/de/dhbw/swe/betterjeweled/core/CrystalRegion.java
@@ -5,6 +5,30 @@ import lombok.*;
 /**
  * The CrystalRegion class represents a simple rectangular shape and offers utility functionality to check whether a
  * field is contained within this region.
+ * <br />
+ * A CrystalRegion is always defined by its starting and ending coordinates. The region coordinates are considered to be
+ * start-inclusive and end-exclusive in both directions.
+ * <br />
+ * Examples (Region coordinates given below the grid in the shape of [StartX, StartY; EndX, EndY]):
+ * <br />
+ * OXO
+ * OXO
+ * OOO
+ * ---
+ * [1, 0; 2, 3]
+ * <br />
+ * XXXO
+ * XXXO
+ * XXXO
+ * XXXO
+ * OOOO
+ * ---
+ * [0, 0; 3, 4]
+ * <br />
+ * OO
+ * OO
+ * ---
+ * [0, 0; 0, 0]
  */
 @Value
 public class CrystalRegion
